f05433feb7c5098e4fa8087949fc048c57959d94
wll8 <xwdaysme@gmail.com>
2021-10-08 16:20:58 +0800

    chore: 开启 css sourceMap

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,15 @@ const config = require(`./common/config.js`) || {}
 
 module.exports = {
   css: {
+    sourceMap: true,
     loaderOptions: {
       scss: {
         // https://vue-loader.vuejs.org/zh/guide/pre-processors.html#共享全局变量
         // 你也可以从一个文件读取，例如 `variables.scss`
         // 如果 sass-loader 版本 = 8，这里使用 `prependData` 字段
         // 如果 sass-loader 版本 < 8，这里使用 `data` 字段
+
+        // 替换 uniapp 默认注入的样式
         prependData: `
           @import "~@/uni.scss";
           $uni-static-url: '${config.staticUrl}';
